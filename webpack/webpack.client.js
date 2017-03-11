345eda7e10853158dcc1c7292a75ca4ba28d4b7e
FrozenPandaz <jasonjean1993@gmail.com>
2017-03-11 14:52:30 -0500

    Add ScriptExtPlugin

--- webpack/webpack.client.js
+++ webpack/webpack.client.js
@@ -4,6 +4,8 @@ const { AotPlugin } = require('@ngtools/webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
+
 /**
  * This is a client config which should be merged on top of common config
  */
@@ -16,7 +18,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: root('./src/index.html'),
-      output: root('dist')
+      output: root('dist'),
+      inject: 'head'
+    }),
+    new ScriptExtPlugin({
+      defaultAttribute: 'defer'
     })
   ]
 };
