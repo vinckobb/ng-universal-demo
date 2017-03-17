ad0cd6e015a29779db134b6dcbb6ff471e7d1173
kukko <kukjevov@gmail.com>
2017-03-17 21:59:15 +0100

    UPDATE - added number directive sample

--- app/pages/+samples/common/commonSamples.component.ts
+++ app/pages/+samples/common/commonSamples.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ComponentRoute, ProgressIndicatorService} from '@ng/common';
+import {ComponentRoute, ProgressIndicatorService, Utils} from '@ng/common';
 
 @Component(
 {
@@ -11,6 +11,16 @@ export class CommonSamplesComponent
 {
     //######################### public properties #########################
     public sampleNumber = 5235342.3231;
+
+    /**
+     * Gets indication whether controls have error
+     */
+    public hasError = Utils.forms.hasError;
+    
+    /**
+     * Gets indication whether hide validations or not for controls
+     */
+    public alertHidden = Utils.forms.alertHidden;
 
     //######################### constructors #########################
     constructor(private progressSvc: ProgressIndicatorService)
