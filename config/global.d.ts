70b5c61841e007f88e44ae7ccc8f3f3c34d18498
kukko <kukjevov@gmail.com>
2017-03-17 17:49:18 +0100

    UPDATE - gulpfile and scripts in pacakge.json

---
+++ config/global.d.ts
@@ -0,0 +1,52 @@
+declare module "config/global"
+{
+    var _tmp:
+    {
+        /**
+         * Indication that application is running in debug mode
+         */
+        debug: boolean;
+        
+        /**
+         * Indication whether enable server logging of js errors
+         */
+        serverLogging: boolean;
+        
+        /**
+         * Version of current running application
+         */
+        version: string;
+        
+        /**
+         * Base url that is used for accessing REST api
+         */
+        apiBaseUrl: string;
+        
+        /**
+         * Object hodling default headers that are send with rest requests
+         */
+        defaultApiHeaders: { [key: string]: any };
+        
+        /**
+         * Default application language
+         */
+        defaultLanguage: string;
+        
+        /**
+         * Default number of items per page when using paging
+         */
+        defaultItemsPerPage: number;
+        
+        /**
+         * Default number of page when used paging
+         */
+        defaultPage: number;
+        
+        /**
+         * Available languages in application
+         */
+        availaleLanguages: [{lang: string; title: string;}];
+    };
+
+    export = _tmp;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      watch = require('gulp-watch'),
       through2 = require('through2'),
       ngAotCompliant = require('gulp-aot-compliant');
 
@@ -19,4 +21,134 @@ gulp.task("create-aot-app", function()
         .pipe(logCopied())
         .pipe(ngAotCompliant())
         .pipe(gulp.dest("app.aot"));
+});
+
+gulp.task("copy-bootstrap-css", function()
+{
+    return gulp.src("node_modules/bootstrap/dist/css/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/bootstrap/dist/css"));
+});
+
+gulp.task("copy-bootstrap-fonts", function()
+{
+    return gulp.src("node_modules/bootstrap/dist/fonts/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/bootstrap/dist/fonts"));
+});
+
+gulp.task("copy-bootstrap-select", function()
+{
+    return gulp.src("node_modules/bootstrap-select/dist/css/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/bootstrap-select/dist/css"));
+});
+
+gulp.task("copy-bootstrap-datepicker", function()
+{
+    return gulp.src("node_modules/eonasdan-bootstrap-datetimepicker/build/css/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/eonasdan-bootstrap-datetimepicker/build/css"));
+});
+
+gulp.task("copy-font-awesome-css", function()
+{
+    return gulp.src("node_modules/font-awesome/css/font-awesome.min.css")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/font-awesome/css"));
+});
+
+gulp.task("copy-font-awesome-fonts", function()
+{
+    return gulp.src("node_modules/font-awesome/fonts/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/node_modules/font-awesome/fonts"));
+});
+
+gulp.task("copy-binaries", 
+          ["copy-font-awesome-css",
+           "copy-font-awesome-fonts",
+           "copy-bootstrap-css",
+           "copy-bootstrap-fonts",
+           "copy-bootstrap-select",
+           "copy-bootstrap-datepicker"], 
+          function(cb)
+{
+    console.log("Node modules have been copied.");
+    
+    cb();
+});
+
+gulp.task("copy-config", function()
+{
+    return gulp.src("config/**/*.json")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/config"));
+});
+
+gulp.task("watch-config", ["copy-config"], function()
+{
+    return watch('config/**/*.json', { events: ['change', 'add'] })
+        .pipe(through2.obj(function(vinyl, enc, cb)
+                           {
+                               if(vinyl.event == 'change' || vinyl.event == 'add')
+                               {
+                                   console.log("File '" + vinyl.basename + "' is being copied.");
+                               }
+                           
+                               this.push(vinyl);
+                               cb();
+                           }))
+        .pipe(gulp.dest('wwwroot/config'));
+});
+
+gulp.task("copy-external-content", function()
+{
+    return gulp.src("content/external/**/*.*")
+        .pipe(logCopied())
+        .pipe(gulp.dest("wwwroot/content/external"));
+});
+
+gulp.task("watch-external-content", ["copy-external-content"], function()
+{
+    return watch('content/external/**/*.*', { events: ['change', 'add'] })
+        .pipe(through2.obj(function(vinyl, enc, cb)
+                           {
+                               if(vinyl.event == 'change' || vinyl.event == 'add')
+                               {
+                                   console.log("File '" + vinyl.basename + "' is being copied.");
+                               }
+                           
+                               this.push(vinyl);
+                               cb();
+                           }))
+        .pipe(gulp.dest('wwwroot/content/external'));
+});
+
+gulp.task("compile-scss", function()
+{
+    return gulp.src('content/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/content'));
+});
+
+gulp.task("build", 
+          ["copy-config",
+           "copy-external-content",
+           "copy-binaries"],
+          function(cb)
+{
+    console.log("Gulp build has finished");
+    
+    cb();
+});
+
+gulp.task("build:aot", 
+          ["build",
+           "create-aot-app"],
+          function(cb)
+{
+    console.log("Gulp build has finished");
+    
+    cb();
 });
