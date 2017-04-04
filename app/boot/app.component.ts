e39c4d61da4b98cf13abc9971ef9ea99753adc7b
kukko <kukjevov@gmail.com>
2017-04-04 19:06:40 +0200

    UPDATE - removed code

--- app/boot/app.component.ts
+++ app/boot/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel} from '@angular/router';
-import {Utils, GlobalizationService, ProgressIndicatorService, CookieService} from '@ng/common';
-import {AuthenticationService} from '@ng/authentication';
 import {TranslateService} from "@ngx-translate/core";
 import {LANG_COOKIE} from '../misc/constants';
 import {Subscription} from 'rxjs/Subscription';
@@ -20,52 +18,14 @@ export class AppComponent implements OnInit
 {
     //######################### private fields #########################
     
-    /**
-     * Subscription for route changes
-     */
-    private _routeChangeSubscription: Subscription;
-
     //######################### constructor #########################
-    constructor(authetication: AuthenticationService<any>,
-                translate: TranslateService,
-                globalization: GlobalizationService,
-                router: Router,
-                progressIndicatorService: ProgressIndicatorService,
-                cookieService: CookieService) 
+    constructor(translate: TranslateService,
+                router: Router) 
     {
-        this._routeChangeSubscription = router.events.subscribe((next) =>
-        {
-            if(next instanceof NavigationStart)
-            {
-                progressIndicatorService.showProgress();
-            }
-            else if(next instanceof NavigationEnd || next instanceof NavigationError || next instanceof NavigationCancel)
-            {
-                progressIndicatorService.hideProgress();
-            }
-        })
-
         var currentLang: string = global.defaultLanguage;
-        var storedCookie = cookieService.getCookie(LANG_COOKIE);
 
-        if(storedCookie)
-        {
-            currentLang = storedCookie;
-        }
-        
-        moment.locale(globalization.getLocale());
         translate.setDefaultLang(currentLang);
         translate.use(currentLang);
-
-        authetication
-            .getUserIdentity()
-            .then(identity =>
-            {
-                if(!identity)
-                {
-                    console.error("User identity was not returned!");
-                }
-            });
     }
 
     //######################### public methods - implementation of OnInit #########################
@@ -84,10 +44,5 @@ export class AppComponent implements OnInit
      */
     public ngOnDestroy()
     {
-        if(this._routeChangeSubscription)
-        {
-            this._routeChangeSubscription.unsubscribe();
-            this._routeChangeSubscription = null;
-        }
     }
 }
--- app/services/api/data/data.interface.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sample data that are received using REST
- */
-export interface Data
-{
-    greeting: string;
-    name: string;
-}
