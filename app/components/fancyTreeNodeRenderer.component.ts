7b6279f0864308cdd0efcbc0dde0dd7e43067d79
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2017-09-13 14:11:24 +0200

    UPDATE - now supports templating and binding all node properties

--- app/components/fancyTreeNodeRenderer.component.ts
+++ app/components/fancyTreeNodeRenderer.component.ts
@@ -9,10 +9,48 @@ import {FancyTreeNodeData} from './fancyTree.interface';
 {
     selector: 'li[fancytree-node-renderer]',
     templateUrl: 'fancyTreeNodeRenderer.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: 
+    {
+        '[attr.data-json]': 'dataJson'
+    }
 })
 export class FancyTreeNodeRendererComponent
 {
+    //######################### public properties - host #########################
+
+    /**
+     * Gets serialized data for node
+     */
+    public get dataJson(): string
+    {
+        if(!this.node)
+        {
+            return null;
+        }
+
+        return JSON.stringify(
+        {
+            active: this.node.active,
+            checkbox: this.node.checkbox,
+            data: this.node.data,
+            expanded: this.node.expanded,
+            extraClasses: this.node.extraClasses,
+            focus: this.node.focus,
+            folder: this.node.folder,
+            icon: this.node.icon,
+            key: this.node.key,
+            lazy: this.node.lazy,
+            refKey: this.node.refKey,
+            selected: this.node.selected,
+            statusNodeType: this.node.statusNodeType,
+            tooltip: this.node.tooltip,
+            unselectable: this.node.unselectable,
+            unselectableIgnore: this.node.unselectableIgnore,
+            unselectableStatus: this.node.unselectableStatus
+        });
+    }
+
     //######################### public properties - inputs #########################
 
     /**
