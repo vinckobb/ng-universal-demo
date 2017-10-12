9baa675b8bd7562cb6cd301dd8118bd5ae50ae2f
kukko <kukjevov@gmail.com>
2017-10-11 18:25:43 -0700

    UPDATE - updated mocks to simulate auth and bad requests

--- server.mock.js
+++ server.mock.js
@@ -678,6 +678,30 @@ module.exports = function(app)
         console.timeEnd(`GET ${req.originalUrl}`);
     });
 
+    app.use('/api/unauthorized', function (req, res, next)
+    {
+        console.time(`GET ${req.originalUrl}`);
+
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 401;
+
+        res.end(null);
+
+        console.timeEnd(`GET ${req.originalUrl}`);
+    });
+
+    app.use('/api/badRequest', function (req, res, next)
+    {
+        console.time(`GET ${req.originalUrl}`);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 400;
+
+        res.end(null);
+
+        console.timeEnd(`GET ${req.originalUrl}`);
+    });
+
     app.use('/api/logout', function (req, res, next)
     {
         console.time(`GET ${req.originalUrl}`);
