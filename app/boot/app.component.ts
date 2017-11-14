ea4a11b29c2722835abffb8cd78804fac57ff160
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2017-11-14 15:17:46 +0100

    UPDATE - now working test check update

--- app/boot/app.component.ts
+++ app/boot/app.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnDestroy} from '@angular/core';
 import {Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel} from '@angular/router';
-import {GlobalizationService, ProgressIndicatorService, CookieService} from '@ng/common';
+import {SwUpdate} from '@angular/service-worker';
+import {GlobalizationService, ProgressIndicatorService, CookieService, APP_STABLE} from '@ng/common';
 import {AuthenticationService} from '@ng/authentication';
 import {TranslateService} from "@ngx-translate/core";
 import {LANG_COOKIE} from '../misc/constants';
+import {interval} from 'rxjs/observable/interval';
 import {Subscription} from 'rxjs/Subscription';
 import * as global from 'config/global';
 import * as moment from 'moment';
@@ -25,14 +27,39 @@ export class AppComponent implements OnDestroy
      */
     private _routeChangeSubscription: Subscription;
 
+    /**
+     * Subscription for update check
+     */
+    private _updateCheckSubscription: Subscription;
+
     //######################### constructor #########################
     constructor(authetication: AuthenticationService<any>,
                 translate: TranslateService,
                 globalization: GlobalizationService,
                 router: Router,
                 progressIndicatorService: ProgressIndicatorService,
-                cookieService: CookieService) 
+                cookieService: CookieService,
+                update: SwUpdate) 
     {
+        update.activated.subscribe(itm => console.log('activated', itm));
+        update.available.subscribe(itm =>
+        { 
+            console.log('available', itm);
+            update.activateUpdate()
+                .then(() => console.log("activated done"));
+        });
+
+        APP_STABLE.then(() =>
+        {
+            this._updateCheckSubscription = interval(30000)
+                .subscribe(time =>
+                {
+                    console.log("checking for update ", time);
+                    update.checkForUpdate()
+                        .then(() => console.log("check done"));
+                });
+        });
+
         this._routeChangeSubscription = router.events.subscribe((next) =>
         {
             if(next instanceof NavigationStart)
@@ -84,6 +111,12 @@ export class AppComponent implements OnDestroy
         {
             this._routeChangeSubscription.unsubscribe();
             this._routeChangeSubscription = null;
+        }
+
+        if(this._updateCheckSubscription)
+        {
+            this._updateCheckSubscription.unsubscribe();
+            this._updateCheckSubscription = null;
         }
     }
 }
