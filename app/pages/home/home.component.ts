c23339810c79939fb826e11669a567061c52fd7a
marek.bukovsky <marek.bukovsky@asseco-ce.com>
2018-11-30 18:24:43 +0100

    INT: commit, experimenting with imports

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import {Authorize, AuthGuard} from '@ng/authentication';
 import {FancyTreeNodeData, FancyTreeComponent} from '@ng/treeview';
 import {GetOptionsCallback, OptionComponent} from '@ng/select';
 import {map} from 'rxjs/operators';
-import {editor, languages} from 'monaco-editor';
+import {editor, languages, Uri} from 'monaco-editor';
 
 import {DataService} from "../../services/api/data/data.service";
 import {BaseAnimatedComponent} from "../../misc/baseAnimatedComponent";
@@ -149,15 +149,84 @@ export class HomeComponent extends BaseAnimatedComponent implements OnInit
         {
             target: languages.typescript.ScriptTarget.ES5,
             module: languages.typescript.ModuleKind.CommonJS,
+            moduleResolution: languages.typescript.ModuleResolutionKind.NodeJs,
             lib: 
             [
-                "es2016",
+                "esnext",
                 "dom"
             ],
-            allowNonTsExtensions: true
+            allowNonTsExtensions: true,
+            typeRoots: ["node_modules/@types"]
         };
-
+        
         languages.typescript.typescriptDefaults.setCompilerOptions(options);
+        languages.typescript.typescriptDefaults.addExtraLib(
+            `export { Observable } from './internal/Observable';
+            export { ConnectableObservable } from './internal/observable/ConnectableObservable';
+            export { GroupedObservable } from './internal/operators/groupBy';
+            export { Operator } from './internal/Operator';
+            export { observable } from './internal/symbol/observable';
+            export { Subject } from './internal/Subject';
+            export { BehaviorSubject } from './internal/BehaviorSubject';
+            export { ReplaySubject } from './internal/ReplaySubject';
+            export { AsyncSubject } from './internal/AsyncSubject';
+            export { asap as asapScheduler } from './internal/scheduler/asap';
+            export { async as asyncScheduler } from './internal/scheduler/async';
+            export { queue as queueScheduler } from './internal/scheduler/queue';
+            export { animationFrame as animationFrameScheduler } from './internal/scheduler/animationFrame';
+            export { VirtualTimeScheduler, VirtualAction } from './internal/scheduler/VirtualTimeScheduler';
+            export { Scheduler } from './internal/Scheduler';
+            export { Subscription } from './internal/Subscription';
+            export { Subscriber } from './internal/Subscriber';
+            export { Notification } from './internal/Notification';
+            export { pipe } from './internal/util/pipe';
+            export { noop } from './internal/util/noop';
+            export { identity } from './internal/util/identity';
+            export { isObservable } from './internal/util/isObservable';
+            export { ArgumentOutOfRangeError } from './internal/util/ArgumentOutOfRangeError';
+            export { EmptyError } from './internal/util/EmptyError';
+            export { ObjectUnsubscribedError } from './internal/util/ObjectUnsubscribedError';
+            export { UnsubscriptionError } from './internal/util/UnsubscriptionError';
+            export { TimeoutError } from './internal/util/TimeoutError';
+            export { bindCallback } from './internal/observable/bindCallback';
+            export { bindNodeCallback } from './internal/observable/bindNodeCallback';
+            export { combineLatest } from './internal/observable/combineLatest';
+            export { concat } from './internal/observable/concat';
+            export { defer } from './internal/observable/defer';
+            export { empty } from './internal/observable/empty';
+            export { forkJoin } from './internal/observable/forkJoin';
+            export { from } from './internal/observable/from';
+            export { fromEvent } from './internal/observable/fromEvent';
+            export { fromEventPattern } from './internal/observable/fromEventPattern';
+            export { generate } from './internal/observable/generate';
+            export { iif } from './internal/observable/iif';
+            export { interval } from './internal/observable/interval';
+            export { merge } from './internal/observable/merge';
+            export { never } from './internal/observable/never';
+            export { of } from './internal/observable/of';
+            export { onErrorResumeNext } from './internal/observable/onErrorResumeNext';
+            export { pairs } from './internal/observable/pairs';
+            export { race } from './internal/observable/race';
+            export { range } from './internal/observable/range';
+            export { throwError } from './internal/observable/throwError';
+            export { timer } from './internal/observable/timer';
+            export { using } from './internal/observable/using';
+            export { zip } from './internal/observable/zip';
+            export { EMPTY } from './internal/observable/empty';
+            export { NEVER } from './internal/observable/never';
+            export * from './internal/types';
+            export { config } from './internal/config';
+            
+            export declare function next() : string;
+            export * from 'rxjs';`,
+            'file:///node_modules/@types/external/index.d.ts');
+
+        languages.typescript.typescriptDefaults.setDiagnosticsOptions(
+        {
+            noSemanticValidation: false,
+            noSyntaxValidation: false
+        });
+
 
         (self as any).MonacoEnvironment = {
             getWorkerUrl: function (moduleId, label) {
@@ -177,18 +246,23 @@ export class HomeComponent extends BaseAnimatedComponent implements OnInit
             }
           }
 
+//         let code = `export class TestClass
+// {
+//     public show()
+//     {
+//         return 'somarina';
+//     }
+// }`;
+
+var code = `import * as x from "external"
+    const tt : string = x.dnext();`;
+
         this.codeEditor = editor.create(document.getElementById('mon'), 
         {
-            value: `export class TestClass
-{
-    public show()
-    {
-        return 'somarina';
-    }
-}`,
             language: 'typescript',
-            theme: 'vs-dark'
-        });
+            theme: 'vs-dark',
+            model: editor.createModel(code, "typescript", Uri.file("file:///index.ts"))
+        }, );
     }
 
     //######################### public methods - implementation of AfterViewInit #########################
